4b272fec870b6667871086615897b9d4f634fe81
C5m7b4 <mbedingfield@dcrpos.com>
2022-03-08 11:45:07 -0600

    initial commit

---
+++ templates/javascript-react/src/index.js
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+console.log('i am loaded');
+
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
